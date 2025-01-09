c26b85bf0a7fae9065357e2a15913b855d7db847
psrtech <contactus.psr@gmail.com>
2025-01-09 20:53:52 +0530

    Written the Steps to do For the Program

---
+++ Main.java
@@ -0,0 +1,15 @@
+// create an 3d array.
+
+// fill the array with minimum value
+
+// craate two players.
+
+// give each player alternate chances to mark in the board.
+
+// mark the values in the board if the user requested place is empty.
+
+// else return error
+
+// check if the player has won the game.
+
+// if the player has won the game, return the player as winner.
